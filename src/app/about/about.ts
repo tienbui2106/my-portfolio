c66d38de70aa82d95d2fa56d9b5219a68a6a6205
Tien Nhat Bui <buinhattien216@gmail.com>
2025-07-28 03:34:59 -0400

    Auto-generated commit

--- src/app/about/about.ts
+++
@@ -1,9 +0,0 @@
-import { AfterViewInit, Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about',
-  imports: [],
-  templateUrl: './about.html',
-  styleUrl: './about.css',
-})
-export class About {}
